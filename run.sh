7500cbaaacc75c649c08ff616e3a74acbb341e19
Yizheng Jiao <yizhengjiao@bytedance.com>
2025-05-26 11:48:50 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+
+cargo build --release
+
+cargo run --bin basic_main
+cargo run --release --bin basic_main  # For release version
+
+cargo run --bin perf_main
+cargo run --release --bin perf_main   # For release version
+
